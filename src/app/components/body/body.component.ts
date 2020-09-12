621d3f178c940712b81072e19cb727adeb8dd69d
IK\prog2 <ervert@ikodin.com>
2020-09-12 13:34:38 -0500

    mm2

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  mostrar = true;
+
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+  }
+
+  personajes: string[] = ['Spiderman', 'Venom', 'Dr. Otopus'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
